3cadaefd1080b488e233ee19fa982502a439ca0d
Carlisa100 <348823873@gapps.yrdsb.ca>
2024-09-12 13:17:07 -0400

    Even More & More Updates

--- Student.java
+++ Student.java
@@ -3,13 +3,9 @@ public class Student{
   public String lastName;
   public int studentAge;
   public String studentBirthday;
-<<<<<<< HEAD
   public String studentFavouriteSubject;
   public int volunteerHours;
   
   System.out.println( firstName + " " + lastName);
   System.out.println( " I am " + studentAge);
-=======
-  public String studentFavouriteSubject
->>>>>>> parent of 6d4edb7 (Updated Information)
 }
